88cea837f3f056f2cf0043e832004c82ebe3bc92
zal-lex <leshik89@gmail.com>
2019-12-23 12:50:39 +0300

    Method to_s updated

--- lib/product_collection.rb
+++ lib/product_collection.rb
@@ -54,10 +54,11 @@ class ProductCollection
   end
 
   def to_s
-    result = ''
-    @products.each.with_index(1) do |product, ind|
-      result += "#{ind}. #{product}\n"
-    end
-    "#{result}"
+    # result = ''
+    # @products.each.with_index(1) do |product, ind|
+    #   result += "#{ind}. #{product}\n"
+    # end
+    # "#{result}"
+    @products.map.with_index(1) {|product, index| "#{index}. #{product}"}.join("\n")
   end
 end
